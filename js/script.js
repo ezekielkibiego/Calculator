a44278ec24b1d3ca5ada97cfe279312dbf6996fa
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-09-01 15:39:33 +0300

    adding the heading to html file

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var add = function(number1, number2) {
+    return number1 + number2;
+  };
+  var number1 = parseInt(prompt("Enter your name:"));
+  var number2 = parseInt(prompt("Enter your last name:"));
+  alert(add(number1, number2));
+  
