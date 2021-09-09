57b086cc9ae4b3559cde645f813a5525c1cf5f1a
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-09-09 08:49:58 +0300

    add result output to js file

--- js/script.js
+++ js/script.js
@@ -11,11 +11,13 @@ var add = function(number1, number2) {
   return number1 / number2;
  };
  
-$(document).ready(function() {
+
+ $(document).ready(function() {
   $("form#add").submit(function(event) {
   event.preventDefault();
   var number1 = parseInt($("#add1").val());
   var number2 = parseInt($("#add2").val());
-  alert(add(number1, number2));
+  var result = add(number1, number2);
+  $("#output").text(result);
   });
   });
